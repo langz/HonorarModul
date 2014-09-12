ca02980ff9304e73e17f471dc527f593ef898c4c
Anders <anders@Anderss-MacBook-Air.local>
2014-09-12 17:42:18 +0200

    test

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name honorarModulApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the honorarModulApp
+ */
+angular.module('honorarModulApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
